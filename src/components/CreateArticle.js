38ba72c44940295f1849f42e0639034e256c42d4
Joff317 <j.freixanet@hotmail.com>
2022-09-07 18:33:47 +0200

    map center + form edit

--- src/components/CreateArticle.js
+++ src/components/CreateArticle.js
@@ -18,35 +18,6 @@ function CreateArticle({ forceUpdate }) {
 		formState: { errors },
 	} = useForm();
 
-	// const updateCoordinate = (lat, lon, article) => {
-	// 	fetch(`${API}articles/${article}`, {
-	// 		method: "PUT",
-	// 		headers: {
-	// 			"Content-type": "application/json",
-	// 			Authorization: `Bearer ${token}`,
-	// 		},
-	// 		body: JSON.stringify({
-	// 			article: {
-	// 				lat,
-	// 				lon,
-	// 			},
-	// 		}),
-	// 	})
-	// 		.then((res) => res.json())
-	// 		.then((data) => {});
-	// };
-
-	// const getCoordinate = (location, articleId) => {
-	// 	fetch(
-	// 		`https://api.geoapify.com/v1/geocode/search?text=${location}&format=json&apiKey=9aa5158850824f25b76a238e1d875cc8`
-	// 	)
-	// 		.then((response) => response.json())
-	// 		.then((data) => {
-	// 			updateCoordinate(data.results[0].lat, data.results[0].lon, articleId);
-	// 		})
-	// 		.catch((err) => console.error(err));
-	// };
-
 	const onSubmit = (data) => {
 		fetch(API + "articles", {
 			method: "POST",
--- src/pages/user/User.js
+++ src/pages/user/User.js
@@ -5,61 +5,75 @@ import { Link } from "react-router-dom";
 import Article from "../../components/article/Article";
 import { logged } from "../../components/atoms/logged";
 import CreateArticle from "../../components/CreateArticle";
+import FormEdit from "../../components/userEdit/FormEdit";
 import { API } from "../../utils/variables";
 import "./user.scss";
 
 function User(props) {
-	const token = Cookies.get("token");
-	const [data, setData] = useState();
-	const loggedd = useAtomValue(logged);
-	const [recucerValue, forceUpdate] = useReducer((x) => x + 1, 0);
-	const isLogged = useAtomValue(logged);
+  const token = Cookies.get("token");
+  const [data, setData] = useState();
+  const loggedd = useAtomValue(logged);
+  const [recucerValue, forceUpdate] = useReducer((x) => x + 1, 0);
+  const isLogged = useAtomValue(logged);
+  const [editing, setEditing] = useState(false)
 
-	useEffect(() => {
-		loggedd &&
-			fetch(API + "member-data", {
-				headers: { Authorization: `Bearer ${token}` },
-			})
-				.then((response) => {
-					return response.json();
-				})
-				.then((res) => {
-					console.log(res);
-					setData(res.user);
-				});
-	}, [loggedd, setData, token, recucerValue]);
+  useEffect(() => {
+    loggedd &&
+      fetch(API + "member-data", {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+        .then((response) => {
+          return response.json();
+        })
+        .then((res) => {
+          console.log(res);
+          setData(res.user);
+        });
+  }, [loggedd, setData, token, recucerValue]);
 
-	return (
-		<div className='flex flex-col justify-center gap-2 relative'>
-			{data && (
-				<>
-					<h1 className='text-2xl font-bold text-center mt-5'>{data.email}</h1>
-					{data.avatar_url && (
-						<img alt={data.avatar.name} src={data.avatar_url} />
-					)}
-					<div id='grid-container'>
-						{isLogged && (
-							<div id='form-container'>
-								<h1>Nouvelle annonce</h1>
-								<CreateArticle forceUpdate={forceUpdate} />
-							</div>
-						)}
+  return (
+    <div className="flex flex-col justify-center gap-2 relative">
+      {data && (
+        <>
+		  { data.avatar.record.name ?
+				 <h1 className="text-2xl font-bold text-center mt-5">{data.avatar.record.name}</h1> 
+				 : 
+				 <h1 className="text-2xl font-bold text-center mt-5">{data.email}</h1>
+		  } 
+          
+           <button onClick={() => setEditing(!editing)}> Editer </button>
+			  {
+				 
+				  editing &&
+				  <FormEdit forceUpdate={forceUpdate} userData={data} />
+				  
+			  }
+          {data.avatar_url && (
+            <img alt={data.avatar.name} src={data.avatar_url} />
+          )}
+          <div id="grid-container">
+            {isLogged && (
+              <div id="form-container">
+                <h1>Nouvelle annonce</h1>
+                <CreateArticle forceUpdate={forceUpdate} />
+              </div>
+            )}
 
-						<div className='flex gap-8 flex-wrap'>
-							{data.articles.map((item) => (
-								<Article
-									key={item.id}
-									article={item}
-									allowEdit={true}
-									forceUpdate={forceUpdate}
-								/>
-							))}
-						</div>
-					</div>
-				</>
-			)}
-		</div>
-	);
+            <div className="flex gap-8 flex-wrap">
+              {data.articles.map((item) => (
+                <Article
+                  key={item.id}
+                  article={item}
+                  allowEdit={true}
+                  forceUpdate={forceUpdate}
+                />
+              ))}
+            </div>
+          </div>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default User;
